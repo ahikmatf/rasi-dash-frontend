d61746686717b2e13ab71657d31ce6675c43e494
Hikmat <asephikmatf@gmail.com>
2019-05-03 10:26:18 +0700

    adding box to every division

--- src/pages/sop-details.js
+++ src/pages/sop-details.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import DivisionCard from '../components/DivisionCard'
+import Heading from 'react-bulma-components/lib/components/heading'
+import Box from 'react-bulma-components/lib/components/box'
 
+import summary from '../data/checklist/summary.json'
 
 class SOPDetails extends Component {
     render() {
         let classId = this.props.match.params.id
+        let className = summary["data"][classId]["class-name"]
 
         return (
             <div>
-                <DivisionCard name={"Production"} classId={classId}/>
-                <DivisionCard name={"Operation"} classId={classId}/>
-                <DivisionCard name={"Marketing"} classId={classId}/>
+                <Heading size={1}>{className}</Heading>
+                <Box>
+                    <DivisionCard name={"Production"} classId={classId}/>
+                </Box>
+                <Box>
+                    <DivisionCard name={"Operation"} classId={classId}/>
+                </Box>
+                <Box>
+                    <DivisionCard name={"Marketing"} classId={classId}/>
+                </Box>
             </div>
         );
     }
